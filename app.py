d8a15f70cffbc9d62e372958f9f9ba2d7c3f877b
anwisu <cabauatan.jianellaguia@gmail.com>
2024-02-24 16:59:40 +0800

    cors

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from userRoutes import configure_jwt_secret, userRoutes
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
+CORS(app, origins='http://localhost:3000')
 app.config['MONGO_URI'] = 'mongodb+srv://jg-cabauatan:EIT7I1S7SCBaCJZO@itp-backend.a0fhlie.mongodb.net/db_kalamansi?retryWrites=true&w=majority'
 app.config['MONGO_DB'] = 'db_kalamansi'
 app.config['JWT_SECRET'] = 'hOBIEr9b5guVvBE5IGBeVqwrBAW7NuUS' 
